dd09ac7213b43b1e1af12adbcec20ab62b2759c0
lam19584 <lam19584@ECS1538-05>
2024-02-29 20:31:08 -0800

    New Changes For Missed Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from "./hello/hello.component";
 import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 
 
@@ -10,7 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HelloComponent, HttpClientModule]
+    imports: [RouterOutlet, HelloComponent, HttpClientModule, NavBarComponent]
 })
 export class AppComponent {
 
